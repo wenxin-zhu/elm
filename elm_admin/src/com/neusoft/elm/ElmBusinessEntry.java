cbe7b4869302f5be3db40298e343c604c398df94
Klein-blue <x2509775929@163.com>
2023-08-24 01:08:18 +0800

    '商家业务流Food--View层与Dao层实现'

--- elm_admin/src/com/neusoft/elm/ElmBusinessEntry.java
+++ elm_admin/src/com/neusoft/elm/ElmBusinessEntry.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 import com.neusoft.elm.po.Business;
 import com.neusoft.elm.view.BusinessView;
 import com.neusoft.elm.view.impl.BusinessViewImpl;
+import com.neusoft.elm.view.FoodView;
+import com.neusoft.elm.view.impl.FoodViewImpl;
+
 public class ElmBusinessEntry {
 	public void work( ) {
 		Scanner input = new Scanner(System.in);
@@ -34,7 +37,7 @@ public class ElmBusinessEntry {
 						 businessView.updateBusinessByPassword(business.getBusinessId());
 					 	break;
 					 case 4:
-						 //foodManager(business.getBusinessId());
+						 foodManager(business.getBusinessId());
 						 break;
 					 case 5:
 						 System.out.println("------------------------欢迎下次光临饿了么后台管理系统-----------------------");
@@ -49,37 +52,42 @@ public class ElmBusinessEntry {
 		}
 	}
 	
-//	private void foodManager(int businessId) {
-//		Scanner input = new Scanner(System.in);
-//		
-//		int menu = 0;
-//		while(menu!=5) {
-//			//二级菜单
-//			System.out.println("\n======= 二级菜单（食品管理）1.查看食品列表=2.新增食品=3.修改食品=4.删除食品=5.返回一级菜单 =======")
-//			System.out.println("请输入你的选择：");
-//			menu = input.nextInt();
-//			
-//			switch(menu) {
-//			 case 1:
-//			 foodView.showFoodList(businessId);
-//			 break;
-//			 case 2:
-//			 foodView.saveFood(businessId);
-//			 break;
-//			 case 3:
-//			 foodView.updateFood(businessId);
-//			 break;
-//			 case 4:
-//			 foodView.removeFood(businessId);
-//			 break;
-//			 case 5:
-//			 break; 
-//			 default:
-//			 System.out.println("没有这个选项！\n");
-//			 break;
-//			 }
-//		}
-//	}
+	private void foodManager(int businessId) {
+		Scanner input = new Scanner(System.in);
+		
+		
+		
+		int menu = 0;
+		while(menu!=5) {
+			//二级菜单
+			System.out.println("\n======= 二级菜单（食品管理）1.查看食品列表=2.新增食品=3.修改食品=4.删除食品=5.返回一级菜单 =======");
+			System.out.println("请输入你的选择：");
+			menu = input.nextInt();
+			
+			FoodView foodView = new FoodViewImpl();
+			
+			
+			switch(menu) {
+			 case 1:
+			 foodView.showFoodList(businessId);
+			 break;
+			 case 2:
+			 foodView.saveFood(businessId);
+			 break;
+			 case 3:
+			 foodView.updateFood(businessId);
+			 break;
+			 case 4:
+			 foodView.removeFood(businessId);
+			 break;
+			 case 5:
+			 break; 
+			 default:
+			 System.out.println("没有这个选项！\n");
+			 break;
+			 }
+		}
+	}
 	public static void main(String[] args) {
 		new ElmBusinessEntry().work();
 	}
