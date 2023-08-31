bdf44f4a4eb874cf46d11fe990ac844346f06a75
zxmhzwx <1728026380@qq.com>
2023-08-31 16:57:59 +0800

    'SpringBoot项目后端点餐业务流1'

---
+++ elmboot/src/test/java/com/neusoft/elmboot/ElmbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neusoft.elmboot;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElmbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
