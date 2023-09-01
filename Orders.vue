cda7cbba86027c125651b1239bc63dd42f7715d2
mzh <1397803513@qq.com>
2023-09-02 00:13:46 +0800

    'servlet业务流3'

---
+++ Orders.vue
@@ -0,0 +1,360 @@
+<template>
+	<div class="wrapper">
+		<!--header部分-->
+		<header>
+			<p>确认订单</p>
+		</header>
+
+		<!--订单信息部分-->
+		<div class="order-info">
+			<h5>订单配送至</h5>
+			<div class="order-info-address" @click="toUserAddress">
+				<p>{{deliveryaddress!=null?deliveryaddress.address:'请选择送货地址'}}</p>
+				<i class="fa fa-angle-right"></i>
+			</div>
+			<p>{{user.userName}}{{formatSex(user.userSex)}} {{user.userId}}</p>
+		</div>
+
+		<h3>{{business.businessName}}</h3>
+
+		<!--订单明细部分-->
+		<ul class="order-detailed">
+			<li v-for="item in cartArr">
+				<div class="order-detailed-left">
+					<img :src="item.food.foodImg">
+					<p>{{item.food.foodName}} x {{item.quantity}}</p>
+				</div>
+				<p>&#165;{{item.food.foodPrice*item.quantity}}</p>
+			</li>
+		</ul>
+		<div class="order-deliveryfee">
+			<p>配送费</p>
+			<p>&#165;{{business.deliveryPrice}}</p>
+		</div>
+
+		<!--合计部分-->
+		<div class="total">
+			<div class="total-left">
+				&#165;{{totalPrice}}
+			</div>
+			<div class="total-right" @click="toPayment">
+				去支付
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	import {
+		ref,
+		onMounted,
+		computed,
+		reactive
+	} from 'vue';
+	import {
+		useRoute,
+		useRouter
+	} from 'vue-router';
+	import axios from 'axios';
+	import qs from 'qs';
+	import {
+		setLocalStorage,
+		getLocalStorage
+	} from '../common.js';
+
+	export default {
+		name: 'Orders',
+		setup() {
+			const route = useRoute();
+			const router = useRouter();
+			const businessId = ref(null);
+			//const business = ref({});
+			const business = ref({});
+			const user = ref(null);
+			const cartArr = ref([]);
+			const deliveryaddress = ref(null);
+
+			// 获取会话存储中的用户数据
+			user.value = JSON.parse(sessionStorage.getItem('user'));
+			//businessId.value = route.query.businessId;
+			// 获取本地存储中的交付地址
+			// const storedDeliveryAddress = localStorage.getItem(user.value.userId);
+			// deliveryaddress.value = storedDeliveryAddress ? JSON.parse(storedDeliveryAddress) : null;
+			deliveryaddress.value = getLocalStorage(user.value.userId);
+
+			onMounted(async () => {
+				//const routeQuery = route.query;
+				businessId.value = route.query.businessId;
+				//user.value = await JSON.parse(sessionStorage.getItem('user'));
+				//getSessionStorage();
+				fetchBusinessInfo();
+				fetchFoodInfo();
+			});
+
+			// const getSessionStorage = () => {
+			// 	const storedUser = sessionStorage.getItem('user');
+			// 	user.value = storedUser ? JSON.parse(storedUser) : null;
+			// };
+
+			//查询当前商家
+			const fetchBusinessInfo = () => {
+				axios
+					.post('BusinessController/getBusinessById', qs.stringify({
+						businessId: businessId.value
+					}))
+					.then(response => {
+						business.value = response.data;
+					})
+					.catch(error => {
+						console.error(error);
+					});
+			};
+			//查询当前用户在购物车中的当前商家食品列表
+			const fetchFoodInfo = () => {
+				axios
+					.post('CartController/listCart', qs.stringify({
+						userId: user.value.userId,
+						businessId: businessId.value
+					}))
+					.then(response => {
+						cartArr.value = response.data;
+					})
+					.catch(error => {
+						console.error(error);
+					});
+			};
+
+			const totalPrice = computed(() => {
+				let total = 0;
+				for (let item of cartArr.value) {
+					total += item.food.foodPrice * item.quantity;
+				}
+				total += business.value.deliveryPrice;
+				return total;
+			});
+
+			const formatSex = (value) => {
+				return value == 1 ? '先生' : '女士';
+			};
+
+			const toUserAddress = () => {
+				router.push({
+					path: '/userAddress',
+					query: {
+						businessId: business.value.businessId
+					}
+				});
+			};
+
+			const toPayment = () => {
+				if (deliveryaddress.value == null) {
+					alert('请选择送货地址！');
+					return;
+				}
+			    axios
+					.post('OrdersController/createOrders', qs.stringify({
+						userId: user.value.userId,
+						businessId: businessId.value,
+						daId: deliveryaddress.value.daId,
+						orderTotal: totalPrice.value
+					}))
+					.then(response => {
+						let orderId = response.data;
+						if (orderId > 0) {
+							router.push({
+								path: '/payment',
+								query: {
+									orderId: orderId
+								}
+							});
+						} else {
+							alert('创建订单失败！');
+						}
+					})
+					.catch(error => {
+						console.error(error);
+					});
+			};
+
+			return {
+				businessId,
+				business,
+				user,
+				cartArr,
+				totalPrice,
+				fetchBusinessInfo,
+				fetchFoodInfo,
+				formatSex,
+				deliveryaddress,
+				toUserAddress,
+				toPayment
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	/****************** 总容器 ******************/
+	.wrapper {
+		width: 100%;
+		height: 100%;
+	}
+
+	/****************** header部分 ******************/
+	.wrapper header {
+		width: 100%;
+		height: 12vw;
+		background-color: #0097FF;
+		color: #fff;
+		font-size: 4.8vw;
+
+		position: fixed;
+		left: 0;
+		top: 0;
+		z-index: 1000;
+
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	/****************** 订单信息部分 ******************/
+	.wrapper .order-info {
+		color: white;
+		width: 100%;
+		margin-top: 12vw;
+		background-color: #0097FF;
+		box-sizing: border-box;
+		padding: 2vw;
+	}
+
+	.wrapper .order-info .h5 {
+		font-size: 3vw;
+		font-weight: 300;
+	}
+
+	.wrapper .order-info .order-info-address {
+		width: 100%;
+		font-weight: 700;
+
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		user-select: none;
+		cursor: pointer;
+		margin: 1vw 0;
+		/*中间一行的上下 左右间距*/
+	}
+
+	.wrapper .order-info .order-info-address p {
+		width: 90%;
+		/*避免地址过长时换行太靠后*/
+		font-size: 5vw;
+	}
+
+	.wrapper .order-info .order-info-address i {
+		font-size: 6vw;
+	}
+
+	.wrapper .order-info p {
+		font-size: 3vw;
+	}
+
+	.wrapper h3 {
+		box-sizing: border-box;
+		padding: 3vw;
+		font-size: 4vw;
+		color: #666;
+		border-bottom: solid 1px #DDD;
+	}
+
+	/****************** 订单明细部分 ******************/
+	.wrapper .order-detailed {
+		width: 100%;
+	}
+
+	.wrapper .order-detailed li {
+		width: 100%;
+		height: 16vw;
+		box-sizing: border-box;
+		padding: 3vw;
+		color: #666;
+
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
+
+	.wrapper .order-detailed li .order-detailed-left {
+		display: flex;
+		align-items: center;
+	}
+
+	.wrapper .order-detailed li .order-detailed-left img {
+		width: 10vw;
+		height: 10vw;
+	}
+
+	.wrapper .order-detailed li .order-detailed-left p {
+		font-size: 3.5vw;
+		margin-left: 3vw;
+	}
+
+	.wrapper .order-detailed li p {
+		font-size: 3.5vw;
+	}
+
+	.wrapper .order-deliveryfee {
+		width: 100%;
+		height: 16vw;
+		box-sizing: border-box;
+		padding: 3vw;
+		color: #666;
+
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		font-size: 3.5vw;
+	}
+
+	/****************** 订单合计部分 ******************/
+	.wrapper .total {
+		width: 100%;
+		height: 14vw;
+
+		position: fixed;
+		/*固定到底部*/
+		left: 0;
+		bottom: 0;
+
+		display: flex;
+
+	}
+
+	.wrapper .total .total-left {
+		flex: 2;
+		background-color: #505051;
+		color: #fff;
+		font-size: 4.5vw;
+		font-weight: 700;
+		user-select: none;
+
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.wrapper .total .total-right {
+		flex: 1;
+		background-color: #38CA73;
+		color: #fff;
+		font-size: 4.5vw;
+		font-weight: 700;
+		user-select: none;
+		cursor: pointer;
+
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+</style>
