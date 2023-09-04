45315731a5c8790b9a88196e2cb737900b1b210b
lj <3498942158@qq.com>
2023-09-04 15:05:08 +0800

    clean

--- elm_admin/src/com/neusoft/elm/dao/AdminDao.java
+++
@@ -1,9 +0,0 @@
-package com.neusoft.elm.dao;
-
-import com.neusoft.elm.po.Admin;
-
-public interface AdminDao {
-	
-	// 根据管理员用户名和密码获取管理员对象
-	public Admin getAdminByNameByPass(String adminName,String password);
-}
